96fe51c11868b72534d7e667416d4e05b69c0d05
alu10711158 <aluqiu1158@ieselcaminas.org>
2023-01-24 17:03:45 +0100

    Initial commit

---
+++ src/main/java/org/ieselcaminas/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.ieselcaminas.helloworld;
+
+/**
+ *
+ * @author alu10711158
+ * 
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
